cf0c698f01cce4d1ae512f0215a89b07c0ee20c8
narugo1992 <narugo@126.com>
2023-10-05 20:07:10 +0800

    dev(narugo): init repo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pynyaasi
-Python client for nyaa.si and sukebei.nyaa.si
+
+Python client for [nyaa.si](https://nyaa.si) and [sukebei.nyaa.si](https://sukebei.nyaa.si)
+
+## Installation
+
+You can simply install it with `pip` command line from the official PyPI site.
+
+```shell
+pip install pynyaasi
+```
---
+++ pynyaasi/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for pynyaasi package.
+"""
+
+#: Title of this project (should be `pynyaasi`).
+__TITLE__ = 'pynyaasi'
+
+#: Version of this project.
+__VERSION__ = '0.0.1'
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Python client for nyaa.si and sukebei.nyaa.si.'
+
+#: Author of this project.
+__AUTHOR__ = 'narugo1992'
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = 'narugo@126.com'
