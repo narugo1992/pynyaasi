73738c01b0220a5ae670082b00e53420986532be
narugo1992 <narugo@126.com>
2023-10-19 20:22:46 +0800

    dev(narugo): add more docs

--- pynyaasi/nyaasi/client.py
+++ pynyaasi/nyaasi/client.py
@@ -7,6 +7,26 @@ from ..client import BaseClient
 
 
 class NyaaSiClient(BaseClient):
+    """
+    A client class for accessing the nyaa.si website.
+
+    The `NyaaSiClient` class is a subclass of `BaseClient` and provides specific functionality for accessing the nyaa.si website.
+
+    :param session: An optional `requests.Session` object to be used for making HTTP requests.
+    :type session: Optional[requests.Session]
+
+    :ivar __endpoint__: The base endpoint URL for the nyaa.si website.
+    :vartype __endpoint__: str
+    :ivar __filter_class__: The available filter options for search queries on nyaa.si.
+    :vartype __filter_class__: FilterType
+    :ivar __default_filter__: The default filter option for search queries on nyaa.si.
+    :vartype __default_filter__: FilterType
+    :ivar __category_class__: The available category options for search queries on nyaa.si.
+    :vartype __category_class__: CategoryType
+    :ivar __default_category__: The default category option for search queries on nyaa.si.
+    :vartype __default_category__: CategoryType
+    """
+
     __endpoint__ = NYAASI_ENDPOINT
     __filter_class__ = FilterType
     __default_filter__ = FilterType.NO_FILTER
@@ -14,4 +34,10 @@ class NyaaSiClient(BaseClient):
     __default_category__ = CategoryType.ALL_CATEGORIES
 
     def __init__(self, session: Optional[requests.Session] = None):
+        """
+        Initialize a new NyaaSiClient instance.
+
+        :param session: An optional `requests.Session` object to be used for making HTTP requests.
+        :type session: Optional[requests.Session]
+        """
         BaseClient.__init__(self, session)
--- pynyaasi/sukebei/client.py
+++ pynyaasi/sukebei/client.py
@@ -7,6 +7,26 @@ from ..client import BaseClient
 
 
 class SukebeiClient(BaseClient):
+    """
+    A client class for accessing the sukebei.nyaa.si website.
+
+    The `SukebeiClient` class is a subclass of `BaseClient` and provides specific functionality for accessing the sukebei.nyaa.si website, which hosts adult content.
+
+    :param session: An optional `requests.Session` object to be used for making HTTP requests.
+    :type session: Optional[requests.Session]
+
+    :ivar __endpoint__: The base endpoint URL for the sukebei.nyaa.si website.
+    :vartype __endpoint__: str
+    :ivar __filter_class__: The available filter options for search queries on sukebei.nyaa.si.
+    :vartype __filter_class__: FilterType
+    :ivar __default_filter__: The default filter option for search queries on sukebei.nyaa.si.
+    :vartype __default_filter__: FilterType
+    :ivar __category_class__: The available category options for search queries on sukebei.nyaa.si.
+    :vartype __category_class__: CategoryType
+    :ivar __default_category__: The default category option for search queries on sukebei.nyaa.si.
+    :vartype __default_category__: CategoryType
+    """
+
     __endpoint__ = SUKEBEI_ENDPOINT
     __filter_class__ = FilterType
     __default_filter__ = FilterType.NO_FILTER
@@ -14,4 +34,10 @@ class SukebeiClient(BaseClient):
     __default_category__ = CategoryType.ALL_CATEGORIES
 
     def __init__(self, session: Optional[requests.Session] = None):
+        """
+        Initialize a new SukebeiClient instance.
+
+        :param session: An optional `requests.Session` object to be used for making HTTP requests.
+        :type session: Optional[requests.Session]
+        """
         BaseClient.__init__(self, session)
